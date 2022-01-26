a5c6d2b2408d461a2cc2bf585f08208e90aad693
Zea <zeaear714@gmail.com>
2022-01-26 16:06:55 +0900

    Camera Split End

--- CameraController/CameraRect.cs
+++ CameraController/CameraRect.cs
@@ -6,45 +6,77 @@ using UnityEngine;
 public class CameraRect : MonoBehaviour{
     [SerializeField] private Camera MainCamera;
     [SerializeField] private Camera[] cameraArray;
+    [SerializeField] private int num;
+    private int depth = 1;
     
 
     void Awake()
-    {
+    {   
+        foreach(Camera i in cameraArray) {
+            i.enabled = false;
+            i.depth = depth;
+            depth += 1;
+        }
         MainCamera.enabled = true;
         ViewRect();
+        if(num > (cameraArray.Length + 1)){
+            Debug.LogWarning("num 설정 제대로");
+            num = 1;
+        } 
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            num = 1;
+            ViewRect();
+        } else if(Input.GetKeyDown(KeyCode.Alpha2)){
+            num = 2;
+            ViewRect();
+        } else if(Input.GetKeyDown(KeyCode.Alpha3)){
+            num = 3;
+            ViewRect();
+        } else if(Input.GetKeyDown(KeyCode.Alpha4)){
+            num = 4;
+            ViewRect();
+        }
+
     }
 
     private void ViewRect()
     {
-        if(cameraArray.Length == 0) ;
-        else if(cameraArray.Length == 1){
+        if(num > (cameraArray.Length + 1)) return;
+        if(num == 1){
+            MainCamera.rect = new Rect(0f, 0f, 1f, 1f);
+            for(int i=0;i<cameraArray.Length;i++){
+                cameraArray[i].enabled = false;
+            }
+        }
+        else if(num == 2){
             MainCamera.rect = new Rect(0.0f, 0f, 0.5f, 1);
             cameraArray[0].rect = new Rect(0.5f, 0f, 1, 1);
-            cameraArray[0].depth = 1;
             cameraArray[0].enabled = true;
+            for(int i=1;i<cameraArray.Length;i++){
+                cameraArray[i].enabled = false;
+            }
         }
-        else if(cameraArray.Length == 2){
+        else if(num == 3){
             MainCamera.rect = new Rect(0.0f, 0, 0.5f, 1);
             cameraArray[0].rect = new Rect(0.5f, 0, 1.0f, 0.5f);
-            cameraArray[0].depth = 1;
-            cameraArray[1].depth = 2;
             cameraArray[1].rect = new Rect(0.5f, 0.5f, 1.0f, 1.0f);
             cameraArray[0].enabled = true;
             cameraArray[1].enabled = true;
+            for(int i=2;i<cameraArray.Length;i++){
+                cameraArray[i].enabled = false;
+            }
         }
-        else if(cameraArray.Length == 3){
+        else if(num == 4){
             MainCamera.rect = new Rect(0.0f, 0.5f, 0.5f, 1);
             cameraArray[2].rect = new Rect(0.5f, 0, 1.0f, 0.5f);
-            cameraArray[0].depth = 1;
-            cameraArray[1].depth = 2;
             cameraArray[0].rect = new Rect(0.5f, 0.5f, 1.0f, 1.0f);
             cameraArray[0].enabled = true;
             cameraArray[1].enabled = true;
             cameraArray[1].rect = new Rect(0.0f, 0f, 0.5f, 0.5f);
-            cameraArray[2].depth = 3;
             cameraArray[2].enabled = true;
-           
-
         }
         
     }
